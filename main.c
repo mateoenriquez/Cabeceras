fbde9351ed1fe62d6bb0833b589e82dc81923290
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-31 14:20:42 -0500

    Biblioteca personalizada agregada

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
+#include "archivo1.h"
+
+int main()
+{
+    
+int a=0, b=0;
+printf("Ingrese el 1er valor: \n");
+scanf("%d", &a);
+printf("Ingrese el 2do valor: \n");
+scanf("%d", &b);
+printf("El resultado de la suma es: %d\n", suma(a,b));
+
+}
+
 
